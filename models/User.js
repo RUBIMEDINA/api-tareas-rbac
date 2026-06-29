50b40ae01b781cbf4631f5f6d97ca124837d6213
RUBIMEDINA <st4512@utr.edu.mx>
2026-06-29 09:07:47 -0600

    Agregado campo activo en User para activar/desactivar usuarios

--- models/User.js
+++ models/User.js
@@ -18,9 +18,17 @@ const UserSchema = new mongoose.Schema({
         type: String,
         enum: ['Admin', 'Supervisor', 'Operador'],
         default: 'Operador'
+    },
+    activo: {
+        type: Boolean,
+        default: true
+    },
+    fechaRegistro: {
+        type: Date,
+        default: Date.now
     }
 }, {
     timestamps: true
 });
 
-module.exports = mongoose.model('User', UserSchema); 
+module.exports = mongoose.model('User', UserSchema);
